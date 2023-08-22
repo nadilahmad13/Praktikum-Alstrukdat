668bbc559d1f983147e4f6d523c0f192cbfd84dc
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-08-22 18:38:15 +0700

    fix: belahketupat, frekuensi

--- Praktikum-1/belahketupat.c
+++ Praktikum-1/belahketupat.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 
+void printstar(int n){
+    int i;
+    for(i = 0; i < n ; i++){
+        printf("*");
+    }
+}
+
+void printspace(int n){
+    int i;
+    for(i = 0; i < n ; i++){
+        printf(" ");
+    }
+}
+
 int main(){
-    // make a diamond shape
-    int i, j, k, l, m, n;
-    int a = 5;
-    int b = 1;
-    int c = 1;
-    int d = 1;
-    int e = 1;
-    int f = 1;
-    int g = 1;
-    
+    // make a diamond shape, input is an odd number
+    // input : 5
+    // output :
+    //     *
+    //    ***
+    //   *****
+    //    ***
+    //     *
+    int n;
+    scanf("%d", &n);
+    int i;
+    for(i = 0; i < n / 2 + 1; i++){
+        printspace(n-i-1);
+        printstar(2*i+1);
+        printf("\n");
+    }
+    for(i = n / 2 - 1; i >= 0; i--){
+        printspace(n-i-1);
+        printstar(2*i+1);
+        printf("\n");
+    }
 }
--- Praktikum-1/frekuensi.c
+++ Praktikum-1/frekuensi.c
@@ -1,76 +1,29 @@
-// Ahmad Nadil
-// 13521024
-// file frekuensi.c
-
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    // variable init
-    char str[100];
-    int arr[10] = {0,0,0,0,0,0,0,0,0,0};
+    char str[10];
+    int angka[10] = {0,0,0,0,0,0,0,0,0,0};
     int i;
-    scanf("%s", str);
 
-    
-    // count the length of str
-    int len = 0;
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+    scanf("%s",str);
 
-    // loop to count the char in the string
-    for (i = 0; i < len; i++)
+    for (i = 0 ; i < strlen(str) ; i++)
     {
-        if (str[i] == '0')
+        if (str[i] >= 48 && str[i] <= 57)
         {
-            arr[0]++;
-        }
-        if (str[i] == '1')
-        {
-            arr[1]++;
-        }
-        else if (str[i] == '2')
-        {
-            arr[2]++;
-        }
-        else if (str[i] == '3')
-        {
-            arr[3]++;
-        }
-        else if (str[i] == '4')
-        {
-            arr[4]++;
-        }
-        else if (str[i] == '5')
-        {
-            arr[5]++;
-        }
-        else if (str[i] == '6')
-        {
-            arr[6]++;
-        }
-        else if (str[i] == '7')
-        {
-            arr[7]++;
-        }
-        else if (str[i] == '8')
-        {
-            arr[8]++;
-        }
-        else if (str[i] == '9')
-        {
-            arr[9]++;
+            angka[str[i] - 48]++;
         }
     }
 
-    for (i = 0; i < 10; i++)
+    for (i = 0 ; i < 10 ; i++)
     {
-        printf("%d", arr[i]);
+        printf("%d",angka[i]);
     }
 
     printf("\n");
 
     return 0;
+
 }
