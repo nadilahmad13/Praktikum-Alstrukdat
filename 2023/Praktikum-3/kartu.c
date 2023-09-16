5250a9636236c52814050461c28ca1746242964a
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-09-16 15:46:31 +0700

    feat: prak 3 2023

--- 2023/Praktikum-3/kartu.c
+++ 2023/Praktikum-3/kartu.c
@@ -1,17 +1,6 @@
 #include "listdin.h"
 #include <stdio.h>
 
-void deleteFirst(ListDin *L, ElType *X)
-{
-    int i;
-    *X = ELMT(*L, 0);
-    for (i = 0; i < listLength(*L) - 1; i++)
-    {
-        ELMT(*L, i) = ELMT(*L, i + 1);
-    }
-    NEFF(*L)--;
-}
-
 int main()
 {
     ListDin L1, L2, L3;
@@ -20,36 +9,36 @@ int main()
     readList(&L1);
     CreateListDin(&L2, 100000);
     readList(&L2);
-    CreateListDin(&L3, 300000);
-    while (!isEmpty(L1) || !isEmpty(L2))
+    CreateListDin(&L3, 200000);
+
+    int idx1 = 0, idx2 = 0;
+    while (idx1 < listLength(L1) || idx2 < listLength(L2))
     {
-        ElType temp;
-        if (isEmpty(L1))
+        if (idx1 == listLength(L1))
         {
-            insertLast(&L3, ELMT(L2, 0));
-            deleteFirst(&L2, &temp);
+            insertLast(&L3, ELMT(L2, idx2));
+            idx2++;
         }
-        else if (isEmpty(L2))
+        else if (idx2 == listLength(L2))
         {
-            insertLast(&L3, ELMT(L1, 0));
-            deleteFirst(&L1, &temp);
+            insertLast(&L3, ELMT(L1, idx1));
+            idx1++;
         }
         else
         {
-            if (ELMT(L1, 0) >= ELMT(L2, 0))
+            if (ELMT(L1, idx1) >= ELMT(L2, idx2))
             {
-                insertLast(&L3, ELMT(L1, 0));
-                deleteFirst(&L1, &temp);
+                insertLast(&L3, ELMT(L1, idx1));
+                idx1++;
             }
             else
             {
-                insertLast(&L3, ELMT(L2, 0));
-                deleteFirst(&L2, &temp);
+                insertLast(&L3, ELMT(L2, idx2));
+                idx2++;
             }
         }
     }
     printList(L3);
     printf("\n");
-
     return 0;
 }
